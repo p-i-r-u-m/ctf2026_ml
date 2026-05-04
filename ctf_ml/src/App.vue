56a9461d9893b0d10d0000b3e0d85c721a1e9706
p-i-r-u-m <igr.case@gmail.com>
2026-05-04 18:26:34 +0300

    created main page

--- ctf_ml/src/App.vue
+++ ctf_ml/src/App.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import logo from './assets/logo.svg'
+import Button from './components/Button.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <div class="mx-auto min-h-screen flex flex-col pt-4 px-4 pb-4 md:pt-8 md:pb-4 md:px-16">
+
+    <header class="flex justify-between items-end pb-128pt">
+      <img :src="logo" alt="Logo" class="h-8 md:h-10 w-auto" />
+    </header>
+
+    <main class="grow">
+      <h2 class="">
+        Motivation letter
+      </h2>
+      <h1 class="pb-16pt">
+        Ivan Hrushevskyi
+      </h1>
+
+      <div class="flex gap-8pt">
+        <Button>Content Responsible</Button>
+        <Button>IT Responsible</Button>
+      </div>
+
+    </main>
+
+    <footer class="mt-32pt pt-16pt border-t border-ui-white/10 font-mono text-sm opacity-40">
+      root@localhost ~ # _
+    </footer>
+
+  </div>
 </template>
---
+++ ctf_ml/src/components/Button.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+// Компонент-кнопка з адаптивним текстом
+</script>
+
+<template>
+  <button
+      class="
+      /* 1. Основа та позиціювання */
+      inline-block cursor-pointer
+
+      /* 2. ТВОЯ АДАПТАЦІЯ ТЕКСТУ (як у <p>) */
+      text-base md:text-lg
+      tracking-tighter
+
+      /* 3. Рамка та кольори */
+      border-[3px] border-ui-accent
+      bg-transparent
+
+      /* 4. Внутрішні відступи (padding) */
+      px-16pt py-8pt
+
+      /* 5. Анімації та Ховер */
+      transition-all
+      hover:bg-ui-accent hover:text-ui-white
+      active:scale-95
+    "
+  >
+    <slot />
+  </button>
+</template>
